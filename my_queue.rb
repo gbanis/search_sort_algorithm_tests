633a773827e30e97cb444c20eebaa823e96993c4
George <gbanis@gmail.com>
2014-12-03 11:50:39 -0500

    Added implemented queue class

--- my_queue.rb
+++ my_queue.rb
@@ -3,8 +3,32 @@ class MyQueue
   # make a new empty cueue
   # add item to the back of queue
   # look at item at front of queue
-  # retrieve and remove item rom front of queue
+  # retrieve and remove item from front of queue
   # ask the size of the queue
   # ask if its empty
+
+  def initialize
+    @q = []
+  end
+
+  def add_item(item)
+    @q.push(item)
+  end
+
+  def look
+    @q[0]
+  end
+
+  def retrieve
+    @q.pop
+  end
+
+  def size
+    @q.length
+  end
+
+  def empty?
+    @q.length > 0
+  end
 
 end
