2145e6c5b6deb7197df8bc48b36609486568f634
George <gbanis@gmail.com>
2014-12-03 11:44:19 -0500

    Added empty my_queue.rb to implement a queue

---
+++ my_queue.rb
@@ -0,0 +1,10 @@
+class MyQueue
+
+  # make a new empty cueue
+  # add item to the back of queue
+  # look at item at front of queue
+  # retrieve and remove item rom front of queue
+  # ask the size of the queue
+  # ask if its empty
+
+end
