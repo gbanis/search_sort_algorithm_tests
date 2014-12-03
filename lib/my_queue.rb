3c9ebceb162e7793f4fc672cc195e96f402a09b2
George <gbanis@gmail.com>
2014-12-03 13:53:39 -0500

    Started MyQueue2 implementation using Nodes and reusing the stack code.

--- lib/my_queue.rb
+++ lib/my_queue.rb
@@ -1,13 +1,6 @@
 class MyQueue
   attr_reader :q
 
-  # make a new empty cueue
-  # add item to the back of queue
-  # look at item at front of queue
-  # retrieve and remove item from front of queue
-  # ask the size of the queue
-  # ask if its empty
-
   def initialize
     @q = []
   end
@@ -30,6 +23,49 @@ class MyQueue
 
   def empty?
     @q.length == 0
+  end
+
+end
+
+class Node
+  attr_accessor :value, :next_node
+
+  def initialize(value = nil)
+    @value = value
+    @next_node = nil
+  end
+
+  def to_s
+    @value || nil
+  end
+end
+
+# Queue implementation with nodes
+
+class MyQueue2
+  attr_accessor :head
+
+  def initialize(first_value=nil)
+    # initializes the linked list
+    @head = Node.new(first_value) if first_value
+  end
+
+  def push(value)
+    # pushes a node into the stack
+    node = Node.new(value)
+    node.next_node = @head
+    @head = node
+  end
+
+  def pop
+    # pops the node from the stack
+    node = @head
+    @head = node.next_node
+    node
+  end
+
+  def peek
+    @head.value
   end
 
 end
--- spec/my_queue_spec.rb
+++ spec/my_queue_spec.rb
@@ -65,3 +65,55 @@ RSpec.describe MyQueue do
   end
 
 end
+
+RSpec.describe MyQueue2 do
+
+  before(:each) do
+    @qq = MyQueue2.new
+    @qq2 = MyQueue2.new(5)
+  end
+
+  describe '#initialize' do
+    it 'creates an empty queue' do
+      expect(@qq.class).to eq MyQueue2
+    end
+
+    it 'creates a queue with a first item if one is present' do
+      expect(@qq2.class).to eq MyQueue2
+      expect(@qq2.head.class).to eq Node
+    end
+
+  end
+
+  describe '#push' do
+    it 'adds a single item to an empty queue' do
+      @qq.push(5)
+      expect(@qq.q).to eq [5]
+    end
+
+    it 'when called twice, adds two items in the correct order' do
+      @qq.push(4)
+      @qq.push(7)
+      expect(@qq.q).to eq [4,7]
+    end
+  end
+
+  describe '#peek' do
+    it 'returns the item at the top of the queue' do
+      @qq.push(4)
+      @qq.push(7)
+      expect(@qq.peek).to eq 4
+    end
+  end
+
+  describe '#pop' do
+    it 'returns the item at the top of the queue and removes it from the queue' do
+      @qq.push(4)
+      @qq.push(7)
+      @qq.push(8)
+      expect(@qq.pop).to eq 4
+      expect(@qq.q).to eq [7,8]
+    end
+  end
+
+end
